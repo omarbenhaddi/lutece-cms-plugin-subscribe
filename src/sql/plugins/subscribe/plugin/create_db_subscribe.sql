8635c17385ed821b5ff5a09c5e9fb76cb6753a64
vbroussard <vbroussard@users.noreply.github.com>
2013-10-15 08:28:44 +0000

    SUBSCRIBE-2 : Updated creation script.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-subscribe@52773 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/subscribe/plugin/create_db_subscribe.sql
+++ src/sql/plugins/subscribe/plugin/create_db_subscribe.sql
@@ -6,7 +6,7 @@
 DROP TABLE IF EXISTS subscribe_subscription;
 CREATE TABLE subscribe_subscription (		
 	id_subscription int(11) NOT NULL default '0',
-	id_subscriber int(11) NOT NULL default '0',
+	id_user varchar(255) NOT NULL default '0',
 	subscription_provider varchar(255) NULL ,
 	subscription_key varchar(255) NOT NULL default '',
 	id_subscribed_resource varchar(255) NOT NULL default '',
