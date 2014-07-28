48cffdb07b92d427fcd8bdd1f3c07db8c06dc5d7
vbroussard <vbroussard@users.noreply.github.com>
2014-07-28 09:29:59 +0000

    SUBSCRIBE-6 : Correction of SQL creation.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-subscribe@70050 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/subscribe/plugin/create_db_subscribe.sql
+++ src/sql/plugins/subscribe/plugin/create_db_subscribe.sql
@@ -5,7 +5,7 @@
 
 DROP TABLE IF EXISTS subscribe_subscription;
 CREATE TABLE subscribe_subscription (		
-	id_subscription int(11) NOT NULL default '0',
+	id_subscription int NOT NULL default '0',
 	id_user varchar(255) NOT NULL default '0',
 	subscription_provider varchar(255) NULL ,
 	subscription_key varchar(255) NOT NULL default '',
